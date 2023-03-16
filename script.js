bf0ad3f575180a830101d2222251c6604df2895f
Keith Roe <kdroe1@gmail.com>
2023-03-16 14:25:47 -0400

    LiteralMathMethodExercise

---
+++ script.js
@@ -0,0 +1,61 @@
+// 1a. 
+let warmHugs = 'Hi, I\'m Olaf and I like warm hugs';
+
+// 1b.
+console.log(warmHugs.toUpperCase());
+
+// 1c.
+warmHugs = warmHugs.replace("like", "love");
+console.log(warmHugs);
+
+// 2a.
+let beenImpaled = 'Oh, look at that. I\'ve been impaled.';
+
+// 2b.
+console.log(beenImpaled.slice(18, 36));
+
+// 3a. 
+const dotDotDot = '...'; 
+console.log(dotDotDot);
+
+// 3b. 
+// fullName = `Keith ${middle} Roe`;
+let skullBones = `I don\'t have a skull ${dotDotDot} or bones`;
+console.log(skullBones);
+
+// 4.
+console.log(Math.PI);
+
+// 5. 
+let randomNumber = Math.floor(Math.random() * 3)+1;
+console.log(randomNumber);
+
+// Bonus 6.
+console.log(' Let It Go!'.repeat(2).toUpperCase());
+
+//Bonus 7a.
+let reindeers = "Reindeers are better than people."
+console.log(reindeers);
+
+// Bonus 7b.
+console.log(reindeers.replace(/ /g, "_"));
+
+// Bonus 8.
+console.log(Math.sqrt(2));
+
+// Bonus 9. 
+let newRandomNumber = Math.floor((Math.random()* 16) + 7);
+console.log(newRandomNumber);
+
+
+
+
+
+
+
+
+
+
+
+
+
